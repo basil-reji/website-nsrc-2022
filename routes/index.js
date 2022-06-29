5b9cc4b7c2fc84874bad70d56efe1229dd5adf45
Basil Reji <basilrejidev@gmail.com>
2022-06-30 01:10:15 +0530

    design theme selected and landing page started

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'NSRC 2022' });
+});
+
+module.exports = router;
